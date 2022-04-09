0e1a4f3c79b5032508aab04f843720ed2a54651a
宮水 三葉 <gnchenmed@gmail.com>
2022-04-09 18:36:24 +0800

    Delete unit_test.jl

--- unit_test.jl
+++
@@ -1,5 +0,0 @@
-using Test
-@testset "test suit for Fisher's exact test" begin
-           @test fisher_exact(1656, 32106, 17466, 251573) ≈ 1.825331208626037*10^(-31)
-           @test fisher_exact(3, 1, 1, 3) ≈ 0.2429
-end;
