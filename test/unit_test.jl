3f21b2436893d99561316a71f739fbc6b2b4439c
宮水 三葉 <gnchenmed@gmail.com>
2022-04-09 18:43:52 +0800

    Update unit_test.jl

--- test/unit_test.jl
+++ test/unit_test.jl
@@ -1,5 +1,5 @@
 using Test
-include("fet.jl")
+include("../fet.jl")
 
 @testset "test suit for Fisher's exact test" begin
            @test fisher_exact(1656, 32106, 17466, 251573) ≈ 1.825331208626037*10^(-31)
